51fe63c6a2839c364dda06778303b8c1d0be8b9f
Ganeshreddy0712 <91110203+Ganeshreddy0712@users.noreply.github.com>
2021-11-30 22:43:02 +0530

    Update 03_minmax.py

--- 03_minmax.py
+++ 03_minmax.py
@@ -5,28 +5,36 @@ form of a tuple of length two. Do not use the built-in functions min or max in i
 
 
 def min_max(number_sequence):
-  pass
+    lst=list(number_sequence)
+    if len(lst)!=1:
+     a=min(lst)
+     b=max(lst)
+     tup=(a,b)
+    else:
+        tup=(lst[0])
+    return tup
+    pass
         
 
 
 class TestMinMax(unittest.TestCase):
   def test_01(self):
-    self.assertEqual(min_max(4,2,8,4,6), (2,8))
+    self.assertEqual(min_max([4,2,8,4,6]), (2,8))
 
   def test_02(self):
-    self.assertEqual(min_max(4,2), (2,4))
+    self.assertEqual(min_max([4,2]), (2,4))
      
   def test_03(self):
-    self.assertEqual(min_max(4,2,-8,4,6), (-8, 6))
+    self.assertEqual(min_max([4,2,-8,4,6]), (-8, 6))
    
   def test_04(self):
-    self.assertEqual(min_max(0,2,8,4,6), (0,8))
+    self.assertEqual(min_max([0,2,8,4,6]), (0,8))
     
   def test_05(self):
-    self.assertEqual(min_max(2,2,2), (2,2))
+    self.assertEqual(min_max([2,2,2]), (2,2))
    
   def test_06(self):
-    self.assertEqual(min_max(8), (8))
+    self.assertEqual(min_max([8]), (8))
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     unittest.main(verbosity=2)
